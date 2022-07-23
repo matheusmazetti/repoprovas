3789eb460a7498af24d4bf6872cb6827a4181c05
matheus <matheus.mazetti.22@gmail.com>
2022-07-22 23:57:29 -0300

    complete login

--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -1,10 +1,10 @@
 import { Router } from 'express';
-import { insertNewUser } from '../controllers/authController.js';
-import { newUserMiddleware } from '../middlewares/authMiddlewares.js';
+import { insertNewUser, login } from '../controllers/authController.js';
+import { loginMiddleware, newUserMiddleware } from '../middlewares/authMiddlewares.js';
 
 const authRouter = Router();
 
 authRouter.post("/sign-up", newUserMiddleware, insertNewUser);
-authRouter.post("/sign-in");
+authRouter.post("/sign-in", loginMiddleware, login);
 
 export default authRouter;
