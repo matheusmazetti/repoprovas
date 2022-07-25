29940386d578a1876b8319f78ff3f3bb9fcc06b4
matheus <matheus.mazetti.22@gmail.com>
2022-07-25 18:22:30 -0300

    all tests

--- tests/examsTests.test.ts
+++ tests/examsTests.test.ts
@@ -30,7 +30,73 @@ describe("POST /tests/new", () => {
 
         expect(status).toEqual(201);
     });
+
+    it("recive 422 with wrong object", async () => {
+        let testObject = {
+            name: "test",
+            pdfUrl: "www.google.com/alala.pdf",
+            category: "Projeto",
+            discipline: "HTML e CSS"
+            
+        }
+        let result = await supertest(app).post("/tests/new").send(testObject).set('Authorization', validToken);
+        let status = result.status;
+
+        expect(status).toEqual(422);
+    });
+
+    it("recive 401 without send token",async () => {
+        let testObject = {
+            name: "test",
+            pdfUrl: "www.google.com/alala.pdf",
+            category: "Projeto",
+            discipline: "HTML e CSS",
+            teacher: "Diego Pinho"
+        }
+        let result = await supertest(app).post("/tests/new").send(testObject);
+        let status = result.status;
+
+        expect(status).toEqual(401);
+    });
+
+    it("recive 409 when teacher, category or discipline does not exist", async () => {
+        let testObject = {
+            name: "test",
+            pdfUrl: "www.google.com/alala.pdf",
+            category: "Projeto",
+            discipline: "HTML e CSS",
+            teacher: "Diego"
+        }
+        let result = await supertest(app).post("/tests/new").send(testObject).set('Authorization', validToken);
+        let status = result.status;
+
+        expect(status).toEqual(409);
+    });
 })
+
+describe("GET /tests/discipline", () => {
+    it("recive tests object", async () => {
+        let result = await supertest(app).get("/tests/discipline").set('Authorization', validToken);
+        expect(result.body);
+    });
+
+    it("recive 401 not sending token", async () => {
+        let result = await supertest(app).get("/tests/discipline")
+        expect(result.status).toEqual(401);
+    });
+});
+
+describe("GET /tests/teacher", () => {
+    it("recive tests object", async () => {
+        let result = await supertest(app).get("/tests/teacher").set('Authorization', validToken);
+        expect(result.body);
+    });
+
+    it("recive 401 not sending token", async () => {
+        let result = await supertest(app).get("/tests/teacher")
+        expect(result.status).toEqual(401);
+    });
+});
 
 afterAll(async () => {
     await prisma.$disconnect();
